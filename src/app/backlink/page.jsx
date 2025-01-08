f28ea34459dcf3d656cdce743b1331d64525365e
satoshisong01 <rudtjrch@naver.com>
2025-01-08 11:46:46 +0900

    백링크 준비중

--- src/app/backlink/page.jsx
+++ src/app/backlink/page.jsx
@@ -3,50 +3,50 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
-  const [loading, setLoading] = useState(false);
+  //   const [query, setQuery] = useState("");
+  //   const [results, setResults] = useState([]);
+  //   const [loading, setLoading] = useState(false);
 
-  const handleSearch = async () => {
-    if (!query) return alert("검색어를 입력하세요.");
-    setLoading(true);
+  //   const handleSearch = async () => {
+  //     if (!query) return alert("검색어를 입력하세요.");
+  //     setLoading(true);
 
-    try {
-      const response = await fetch(
-        `/api/search-naver?query=${encodeURIComponent(query)}`
-      );
-      const data = await response.json();
-      console.log("data", data);
+  //     try {
+  //       const response = await fetch(
+  //         `/api/search-naver?query=${encodeURIComponent(query)}`
+  //       );
+  //       const data = await response.json();
+  //       console.log("data", data);
 
-      if (data.items) {
-        setResults(data.items);
+  //       if (data.items) {
+  //         setResults(data.items);
 
-        // 검색어가 결과 제목이나 설명에 포함되었는지 확인
-        const match = data.items.some(
-          (item) =>
-            item.title.replace(/<[^>]+>/g, "").includes(query.trim()) || // 제목에 검색어 포함 여부
-            item.description.includes(query.trim()) // 설명에 검색어 포함 여부
-        );
+  //         // 검색어가 결과 제목이나 설명에 포함되었는지 확인
+  //         const match = data.items.some(
+  //           (item) =>
+  //             item.title.replace(/<[^>]+>/g, "").includes(query.trim()) || // 제목에 검색어 포함 여부
+  //             item.description.includes(query.trim()) // 설명에 검색어 포함 여부
+  //         );
 
-        if (match) {
-          alert("O");
-        } else {
-          alert("X");
-        }
-      } else {
-        alert("결과를 가져오지 못했습니다.");
-      }
-    } catch (error) {
-      alert("오류 발생: " + error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+  //         if (match) {
+  //           alert("O");
+  //         } else {
+  //           alert("X");
+  //         }
+  //       } else {
+  //         alert("결과를 가져오지 못했습니다.");
+  //       }
+  //     } catch (error) {
+  //       alert("오류 발생: " + error.message);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
   return (
     <div>
-      <h1>네이버 웹문서 검색</h1>
-      <input
+      <h1>준비중</h1>
+      {/* <input
         type="text"
         placeholder="검색어 또는 URL을 입력하세요"
         value={query}
@@ -69,7 +69,7 @@ export default function Home() {
             ))}
           </ul>
         )}
-      </div>
+      </div> */}
     </div>
   );
 }
