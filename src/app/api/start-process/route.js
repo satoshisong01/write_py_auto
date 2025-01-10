17b29d40c284d0f5739fa1801297c281b372ab4c
satoshisong01 <rudtjrch@naver.com>
2025-01-10 19:10:47 +0900

    아 일단 다시 복구

--- src/app/api/start-process/route.js
+++ src/app/api/start-process/route.js
@@ -1,16 +1,12 @@
-// app/api/start-process/route.js
+// pages/api/start-process/route.js
 
 import axios from "axios";
 import { NextResponse } from "next/server";
 import dotenv from "dotenv";
-import { spawn } from "child_process";
 
 dotenv.config();
 
-// 프로세스 상태를 추적하는 전역 변수
-let processRunning = false;
-
-export async function POST(request) {
+export async function POST() {
   try {
     const localServerUrl = process.env.LOCAL_SERVER_URL;
     const authToken = process.env.AUTH_TOKEN;
@@ -18,14 +14,6 @@ export async function POST(request) {
     if (!localServerUrl || !authToken) {
       throw new Error(
         "LOCAL_SERVER_URL 또는 AUTH_TOKEN이 설정되지 않았습니다."
-      );
-    }
-
-    // 프로세스가 이미 실행 중인지 확인
-    if (processRunning) {
-      return NextResponse.json(
-        { success: false, message: "이미 작업이 실행 중입니다." },
-        { status: 400 }
       );
     }
 
@@ -44,31 +32,31 @@ export async function POST(request) {
       throw new Error(`로그 초기화 실패: ${clearLogsResponse.data.message}`);
     }
 
-    // Step 2: Start automate.py
-    processRunning = true; // 프로세스 시작 전 상태 업데이트
+    // Step 2: Start automate.js
+    const startProcessResponse = await axios.post(
+      `${localServerUrl}/start-process`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    );
 
-    const automationProcess = spawn("python", [
-      "automation.py",
-      "worklist.json",
-    ]);
-
-    automationProcess.stdout.on("data", (data) => {
-      console.log(`automation.py stdout: ${data}`);
-    });
-
-    automationProcess.stderr.on("data", (data) => {
-      console.error(`automation.py stderr: ${data}`);
-    });
-
-    automationProcess.on("close", (code) => {
-      console.log(`automation.py exited with code ${code}`);
-      processRunning = false; // 프로세스 종료 후 상태 초기화
-    });
-
-    return NextResponse.json({
-      success: true,
-      message: "작업이 시작되었습니다!",
-    });
+    if (startProcessResponse.data.success) {
+      return NextResponse.json({
+        success: true,
+        message: "작업이 시작되었습니다!",
+      });
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: startProcessResponse.data.message,
+        },
+        { status: 500 }
+      );
+    }
   } catch (error) {
     console.error("작업 시작 중 오류:", error.message);
     return NextResponse.json(
